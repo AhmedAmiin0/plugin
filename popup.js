b9095e39282ebf0d16d6170b27ef57713fb08a5f
AhmedAmin00 <ahmedamin1925@gmail.com>
2024-05-25 23:20:06 +0300

    changes

--- popup.js
+++ popup.js
@@ -2,12 +2,28 @@ console.log("from popup");
 let startBtn = document.getElementById("start");
 cancelBtn = document.getElementById("cancel");
 loaderContainer = document.getElementById("loader-container");
+const unitNumber = document.getElementById("unitNumber");
+const bookByNumber = document.getElementById("bookByNumber");
+
+const stopReservationKey = "stopReservation";
+let stopReservation = localStorage.getItem(stopReservationKey) === "true";
+if (stopReservation) {
+  console.log("Popup : Reservation stopped running ");
+  hideLoader();
+} else {
+  console.log("Popup : Reservation still running ");
+  displayLoader();
+}
+let getByRequestNumber = false;
 startBtn.addEventListener("click", () => {
-  displayLoader()
+  displayLoader();
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(
       tabs[0].id,
-      { action: "invokeBookFunction" },
+      {
+        action: getByRequestNumber ? "bookByNumber" : "invokeBookFunction",
+        message: getByRequestNumber ? unitNumber.value : null,
+      },
       function (response) {
         console.log(response);
       }
@@ -34,3 +50,13 @@ function hideLoader() {
     (cancelBtn.style.display = "none"),
     chrome.storage.local.set({ started: !1 });
 }
+
+bookByNumber.addEventListener("change", function (e) {
+  if (e.target.checked) {
+    unitNumber.style.display = "block";
+
+    getByRequestNumber = true;
+  } else {
+    unitNumber.style.display = "none";
+  }
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,9 @@
+const stopReservationKey = "stopReservation";
+let stopReservation = localStorage.getItem(stopReservationKey) === "true";
+console.log("stopReservation", stopReservation);
+
 async function book() {
+  localStorage.setItem(stopReservationKey, "true");
   try {
     let url = window.location.href;
     let projectId = url.split("/")[5];
@@ -9,11 +14,6 @@ async function book() {
         withCredentials: true,
       }
     ).then((res) => res.json());
-
-    if (response.status === 401) {
-      chrome.runtime.sendMessage({ action: "PleaseLoginNotification" });
-      return;
-    }
 
     if (response.data.length === 0) {
       chrome.runtime.sendMessage({ action: "noLandsAvailable" });
@@ -31,16 +31,15 @@ async function book() {
       chunkedUnits.push(temparray);
     }
 
-    let canBook = true;
+    let canBook = [];
     for (let chunk of chunkedUnits) {
       let i = 0;
-      if (!canBook) break;
+      if (canBook.some((e) => e === true)) break;
       for (let unit of chunk) {
-        if (!canBook) break;
+        if (canBook.some((e) => e === true)) break;
+        canBook[i] = await validateUserCanBook(projectId);
 
-        canBook = await validateUserCanBook(projectId);
-
-        if (canBook) {
+        if (canBook[i] === true) {
           await fetch(
             `https://sakani.sa/mainIntermediaryApi/v4/units/reserve`,
             {
@@ -73,46 +72,126 @@ async function book() {
     console.log(e);
     chrome.runtime.sendMessage({ action: "SomethingWentWrong" });
   } finally {
+    localStorage.setItem(stopReservationKey, "false");
     chrome.runtime.sendMessage({ action: "stop-loader" });
   }
 }
 
 async function validateUserCanBook(id) {
-  const firstRequest =
-    "https://sakani.sa/mainIntermediaryApi/v4/bookings/precondition_check";
-  const response = await fetch(firstRequest, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      data: {
-        attributes: {
-          national_id_number: "F45D425E378FF8B237248FE032F6DAA9",
-          project_id: id,
-        },
-        id: "beneficiary_sessions",
+  let canBook = false;
+  try {
+    const firstRequest =
+      "https://sakani.sa/mainIntermediaryApi/v4/bookings/precondition_check";
+    const response = await fetch(firstRequest, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    }),
-    withCredentials: true,
-  }).then((res) => res.json());
-
-  const secondReq = await fetch(
-    `https://sakani.sa/sakani-queries-service/cqrs-res?topic=booking_precondition_check_completed&request_id=${response.data.request_id}`,
-    {
+      body: JSON.stringify({
+        data: {
+          attributes: {
+            national_id_number: "F45D425E378FF8B237248FE032F6DAA9",
+            project_id: id,
+          },
+          id: "beneficiary_sessions",
+        },
+      }),
       withCredentials: true,
+    }).then((res) => res.json());
+    // .catch((e) => {
+    // });
+    if (response.status === 401) {
+      chrome.runtime.sendMessage({ action: "PleaseLoginNotification" });
+      throw new Error("Please Login");
     }
-  ).then((res) => res.json());
-  if (secondReq.data?.block_booking_reason?.length === 0) {
-    return true;
+    const secondReq = await fetch(
+      `https://sakani.sa/sakani-queries-service/cqrs-res?topic=booking_precondition_check_completed&request_id=${response.data.request_id}`,
+      {
+        withCredentials: true,
+      }
+    ).then((res) => res.json());
+    if (secondReq.status === 401) {
+      chrome.runtime.sendMessage({ action: "PleaseLoginNotification" });
+      throw new Error("Please Login");
+    }
+    if (secondReq.data?.block_booking_reason?.length === 0) {
+      canBook = true;
+    }
+  } catch (e) {
+    return canBook;
   }
-  return false;
+}
+async function bookWithRequestNumber(requestNumber = 10) {
+  localStorage.setItem(stopReservationKey, "true");
+  try {
+    let url = window.location.href;
+    let projectId = url.split("/")[5];
+
+    const response = await fetch(
+      `https://sakani.sa/marketplaceApi/search/v1/projects/${projectId}/available-units`,
+      {
+        withCredentials: true,
+      }
+    ).then((res) => res.json());
+
+    if (response.data.length === 0) {
+      chrome.runtime.sendMessage({ action: "noLandsAvailable" });
+      return;
+    }
+    chrome.runtime.sendMessage({ action: "StartRerveNotification" });
+    let units = response.data;
+
+    if (units.length >= requestNumber) {
+      // get the first requestNumber units
+      units = units.slice(0, requestNumber);
+      await Promise.all(
+        units.map(async (unit) => {
+          await fetch(
+            `https://sakani.sa/mainIntermediaryApi/v4/units/reserve`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                data: {
+                  attributes: {
+                    unit_code: unit.attributes.unit_code,
+                  },
+                },
+              }),
+              withCredentials: true,
+            }
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+            });
+        })
+      );
+    }
+    chrome.runtime.sendMessage({ action: "bookingCompleted" });
+  } catch (e) {
+    console.log(e);
+    chrome.runtime.sendMessage({ action: "SomethingWentWrong" });
+  } finally {
+    chrome.runtime.sendMessage({ action: "stop-loader" });
+    localStorage.setItem(stopReservationKey, "false");
+  }
 }
 
 // In content.js
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "invokeBookFunction") {
-    book(); // Call the book function
+    book();
     sendResponse({ message: "Book function invoked" });
+  }
+  if (message.action === "bookByNumber") {
+    bookWithRequestNumber(message.message);
+    sendResponse({ message: "Book function invoked" });
+  }
+  if (message.action === "cancel") {
+    localStorage.setItem(stopReservationKey, "false");
+    console.log('a7aaaa')
   }
 });
